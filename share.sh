0e53ed14b5861840f8e439a2e76c843597f68a5d
aexbit <admin@myss.me>
2023-04-07 19:34:01 +0800

    new file:   .env 	deleted:    1 	new file:   docker-compose.yml 	new file:   share.sh

---
+++ share.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "请输入nodeid没有的话请先私聊站长："
+read new_nodeid
+
+echo "请输入运行端口："
+read new_port
+
+echo "请确认您要将nodeid设置为$new_nodeid ,端口设置为$new_port (y/n)"
+read confirm
+
+if [ "$confirm" == "y" ] || [ "$confirm" == "Y" ]; then
+  sed -i "s/nodeId=.*/nodeId=$new_nodeid/g" .env
+  sed -i "s/runPort=.*/runPort=$new_port/g" .env
+  echo "修改已完成"
+else
+  echo "取消修改"
+fi
+docker-compose up
