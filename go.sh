8ac05ced4c59d531972920f3d4b63dd7e7480adb
jackma778 <36823039+jackma778@users.noreply.github.com>
2023-12-11 14:42:24 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -24,6 +24,7 @@ ERROR_IF_UPTODATE=''
 
 CUR_VER=""
 NEW_VER="v4.45.2"
+DOWNLOAD_LINK="https://github.com/v2fly/v2ray-core/releases/download/${NEW_VER}/v2ray-linux-64.zip"
 VDIS=''
 ZIPFILE="/tmp/v2ray/v2ray.zip"
 V2RAY_RUNNING=0
@@ -169,20 +170,6 @@ zipRoot() {
     '
 }
 
-downloadV2Ray(){
-    rm -rf /tmp/v2ray
-    mkdir -p /tmp/v2ray
-    DOWNLOAD_LINK="https://github.com/v2fly/v2ray-core/releases/download/${NEW_VER}/v2ray-linux-64.zip"
-    colorEcho ${BLUE} "Downloading V2Ray: ${DOWNLOAD_LINK}"
-    wget -P /tmp/v2ray "https://github.com/jackma778/sh/releases/download/v0.1/v2scar"
-    curl ${PROXY} -L -H "Cache-Control: no-cache" -o ${ZIPFILE} ${DOWNLOAD_LINK} 
-    if [ $? != 0 ];then
-        colorEcho ${RED} "Failed to download! Please check your network or try again."
-        return 3
-    fi
-    return 0
-}
-
 installSoftware(){
     COMPONENT=$1
     if [[ -n `command -v $COMPONENT` ]]; then
@@ -247,54 +234,82 @@ restartV2ray(){
 }
 
 installV2Ray(){
-  api=https://api.cjy.me
-  token=MJJ6688
-echo "https://share.cjy.me 添加主机信息后，再继续操作本脚本" 
-echo "请输入节点id（和您刚刚在网页填写的节点id保持一致）："
-read new_nodeid
+    api=https://api.cjy.me
+    token=MJJ6688
+    echo "https://share.cjy.me 添加主机信息后，再继续操作本脚本" 
+    echo "请输入节点id（和您刚刚在网页填写的节点id保持一致）："
+    read new_nodeid
+    
+    echo "请确认您要将nodeid设置为$new_nodeid(y/n)"
+    read confirm
 
-echo "请确认您要将nodeid设置为$new_nodeid(y/n)"
-read confirm
+    if [ "$confirm" == "y" ] || [ "$confirm" == "Y" ]; then
+        nodeId=$new_nodeid 
 
-if [ "$confirm" == "y" ] || [ "$confirm" == "Y" ]; then
-  nodeId=$new_nodeid
-  echo "优化内核参数"
-  echo "net.ipv4.tcp_retries2 = 8
-  net.ipv4.tcp_slow_start_after_idle = 0
-  fs.file-max = 1000000
-  fs.inotify.max_user_instances = 8192
-  net.ipv4.tcp_syncookies = 1
-  net.ipv4.tcp_fin_timeout = 30
-  net.ipv4.tcp_tw_reuse = 1
-  net.ipv4.ip_local_port_range = 1024 65000
-  net.ipv4.tcp_max_syn_backlog = 16384
-  net.ipv4.tcp_max_tw_buckets = 6000
-  net.ipv4.route.gc_timeout = 100
-  net.ipv4.tcp_syn_retries = 1
-  net.ipv4.tcp_synack_retries = 1
-  net.core.somaxconn = 32768
-  net.core.netdev_max_backlog = 32768
-  net.ipv4.tcp_timestamps = 0
-  net.ipv4.tcp_max_orphans = 32768
-  net.ipv4.ip_forward = 1" >>/etc/sysctl.conf
-  echo "*               soft    nofile           1000000
-  *               hard    nofile          1000000" >/etc/security/limits.conf
-  echo "ulimit -SHn 1000000" >>/etc/profile
-  source /etc/profile
-  sysctl -p
-    # Install V2Ray binary to /usr/bin/v2ray
-    mkdir -p '/etc/v2ray' '/var/log/v2ray' && \
-    unzip -oj "$1" "$2v2ray" "$2v2ctl" "$2geoip.dat" "$2geosite.dat" -d '/usr/bin/v2ray' && \
-    mv /tmp/v2ray/v2scar /usr/bin/v2ray/ 
-    chmod +x '/usr/bin/v2ray/v2scar' '/usr/bin/v2ray/v2ray' '/usr/bin/v2ray/v2ctl' || {
-        colorEcho ${RED} "Failed to copy V2Ray binary and resources."
-        return 1
-    }
+        # 优化内核参数 检查是否存在重复配置项
+        if ! grep -q "net.ipv4.tcp_congestion_control = bbr" /etc/sysctl.conf; then
+			echo "net.ipv4.tcp_retries2 = 8
+			net.ipv4.tcp_slow_start_after_idle = 0
+			fs.file-max = 1000000
+			net.core.default_qdisc = fq
+			net.ipv4.tcp_congestion_control = bbr
+			fs.inotify.max_user_instances = 8192
+			net.ipv4.tcp_syncookies = 1
+			net.ipv4.tcp_fin_timeout = 30
+			net.ipv4.tcp_tw_reuse = 1
+			net.ipv4.ip_local_port_range = 1024 65000
+			net.ipv4.tcp_max_syn_backlog = 16384
+			net.ipv4.tcp_max_tw_buckets = 6000
+			net.ipv4.route.gc_timeout = 100
+			net.ipv4.tcp_syn_retries = 1
+			net.ipv4.tcp_synack_retries = 1
+			net.core.somaxconn = 32768
+			net.core.netdev_max_backlog = 32768
+			net.ipv4.tcp_timestamps = 0
+			net.ipv4.tcp_max_orphans = 32768
+			net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+            sysctl -p
+        fi
 
-    # Install V2Ray.service and v2scar.service
-    if [[ -n "${SYSTEMCTL_CMD}" ]]; then
-		unzip -oj "$1" "$2systemd/system/v2ray.service" -d '/etc/systemd/system' && sed -i "s@ExecStart=.*@ExecStart=/usr/bin/v2ray/v2ray -config $api/api/vmess_server_config/$nodeId/?token=$token@" /etc/systemd/system/v2ray.service
-		cat <<EOF > /etc/systemd/system/v2scar.service
+        # 优化系统参数 检查是否存在重复配置项
+        if ! grep -q "*               soft    nofile           1000000" /etc/security/limits.conf; then
+            echo "*               soft    nofile           1000000
+            *               hard    nofile          1000000" > /etc/security/limits.conf
+        fi
+        
+        # 优化系统参数 检查是否存在重复配置项
+        if ! grep -q "ulimit -SHn 1000000" /etc/profile; then
+            echo "ulimit -SHn 1000000" >> /etc/profile
+            source /etc/profile
+        fi
+
+        # Download V2ray
+        rm -rf /tmp/v2ray
+        mkdir -p /tmp/v2ray
+        colorEcho ${BLUE} "Downloading V2Ray: ${DOWNLOAD_LINK}"
+        wget -P /tmp/v2ray "https://github.com/jackma778/sh/releases/download/v0.1/v2scar"
+        if [ $? != 0 ]; then
+            colorEcho ${RED} "Failed to download V2Ray! Please check your network or try again."
+            return 3
+        fi
+
+        curl ${PROXY} -L -H "Cache-Control: no-cache" -o ${ZIPFILE} ${DOWNLOAD_LINK} 
+        if [ $? != 0 ]; then
+            colorEcho ${RED} "Failed to download V2Ray! Please check your network or try again."
+            return 3
+        fi
+
+        # Install V2Ray binary to /usr/bin/v2ray
+        mkdir -p '/etc/v2ray' '/var/log/v2ray' && unzip -oj "$1" "$2v2ray" "$2v2ctl" "$2geoip.dat" "$2geosite.dat" -d '/usr/bin/v2ray' && mv /tmp/v2ray/v2scar /usr/bin/v2ray/ && chmod +x '/usr/bin/v2ray/v2scar' '/usr/bin/v2ray/v2ray' '/usr/bin/v2ray/v2ctl' || {
+            colorEcho ${RED} "Failed to copy V2Ray binary and resources."
+            return 1
+        }
+
+        # Install V2Ray.service and v2scar.service
+        if [[ -n "${SYSTEMCTL_CMD}" ]]; then
+            unzip -oj "$1" "$2systemd/system/v2ray.service" -d '/etc/systemd/system' && sed -i "s@ExecStart=.*@ExecStart=/usr/bin/v2ray/v2ray -config $api/api/vmess_server_config/$nodeId/?token=$token@" /etc/systemd/system/v2ray.service
+
+            cat <<EOF > /etc/systemd/system/v2scar.service
 [Unit]
 Description=v2scar
 [Service]
@@ -304,13 +319,20 @@ User=root
 [Install]
 WantedBy=multi-user.target
 EOF
-		systemctl daemon-reload && systemctl enable v2ray.service && systemctl enable v2scar.service && systemctl start v2ray.service && systemctl start v2scar.service
-	else
-		echo "没有找到systemctl命令 无法配置启动脚本 请检查您的系统版本是否过老 建议使用debian10 64位"
+
+            systemctl daemon-reload
+            systemctl enable v2ray.service
+            systemctl enable v2scar.service
+            systemctl start v2ray.service
+            systemctl start v2scar.service
+            colorEcho ${GREEN} "Install successfully."
+        else
+            colorEcho ${RED} "Failed to remove V2Ray, Try use debian10 64"
+            return 2
+        fi
+    else
+        colorEcho ${RED} "Cancel Install."
     fi
-else
-  echo "取消安装"
-fi
 }
 
 
@@ -353,7 +375,6 @@ case "$num" in
 	installSoftware wget
 	installSoftware unzip
 	installSoftware ca-certificates
-	downloadV2Ray
 	installV2Ray "${ZIPFILE}" "${ZIPROOT}" || return $?
         ;;
         1)
@@ -366,6 +387,6 @@ case "$num" in
 	remove
 	    ;;
         *)
-	echo "请输入正确数字 [0-6]"
+	echo "请输入正确数字 [0-4]"
 	;;
 esac
