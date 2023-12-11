24e637e100a24eff708b40a936553052cb6c8371
jackma778 <36823039+jackma778@users.noreply.github.com>
2023-12-11 14:41:44 +0800

    Update docker_go.sh

--- docker_go.sh
+++ docker_go.sh
@@ -22,30 +22,42 @@ echo "请确认您要将nodeid设置为$new_nodeid ,端口设置为$new_port (y/
 read confirm
 
 if [ "$confirm" == "y" ] || [ "$confirm" == "Y" ]; then
-  echo "优化内核参数"
-  echo "net.ipv4.tcp_retries2 = 8
-  net.ipv4.tcp_slow_start_after_idle = 0
-  fs.file-max = 1000000
-  fs.inotify.max_user_instances = 8192
-  net.ipv4.tcp_syncookies = 1
-  net.ipv4.tcp_fin_timeout = 30
-  net.ipv4.tcp_tw_reuse = 1
-  net.ipv4.ip_local_port_range = 1024 65000
-  net.ipv4.tcp_max_syn_backlog = 16384
-  net.ipv4.tcp_max_tw_buckets = 6000
-  net.ipv4.route.gc_timeout = 100
-  net.ipv4.tcp_syn_retries = 1
-  net.ipv4.tcp_synack_retries = 1
-  net.core.somaxconn = 32768
-  net.core.netdev_max_backlog = 32768
-  net.ipv4.tcp_timestamps = 0
-  net.ipv4.tcp_max_orphans = 32768
-  net.ipv4.ip_forward = 1" >>/etc/sysctl.conf
-  echo "*               soft    nofile           1000000
-  *               hard    nofile          1000000" >/etc/security/limits.conf
-  echo "ulimit -SHn 1000000" >>/etc/profile
-  source /etc/profile
-  sysctl -p
+    # 优化内核参数 检查是否存在重复配置项
+    if ! grep -q "net.ipv4.tcp_congestion_control = bbr" /etc/sysctl.conf; then
+		echo "net.ipv4.tcp_retries2 = 8
+		net.ipv4.tcp_slow_start_after_idle = 0
+		fs.file-max = 1000000
+		net.core.default_qdisc = fq
+		net.ipv4.tcp_congestion_control = bbr
+		fs.inotify.max_user_instances = 8192
+		net.ipv4.tcp_syncookies = 1
+		net.ipv4.tcp_fin_timeout = 30
+		net.ipv4.tcp_tw_reuse = 1
+		net.ipv4.ip_local_port_range = 1024 65000
+		net.ipv4.tcp_max_syn_backlog = 16384
+		net.ipv4.tcp_max_tw_buckets = 6000
+		net.ipv4.route.gc_timeout = 100
+		net.ipv4.tcp_syn_retries = 1
+		net.ipv4.tcp_synack_retries = 1
+		net.core.somaxconn = 32768
+		net.core.netdev_max_backlog = 32768
+		net.ipv4.tcp_timestamps = 0
+		net.ipv4.tcp_max_orphans = 32768
+		net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+        sysctl -p
+    fi
+
+    # 优化系统参数 检查是否存在重复配置项
+    if ! grep -q "*               soft    nofile           1000000" /etc/security/limits.conf; then
+        echo "*               soft    nofile           1000000
+        *               hard    nofile          1000000" > /etc/security/limits.conf
+    fi
+    
+    # 优化系统参数 检查是否存在重复配置项
+    if ! grep -q "ulimit -SHn 1000000" /etc/profile; then
+        echo "ulimit -SHn 1000000" >> /etc/profile
+        source /etc/profile
+    fi
   sed -i "s/nodeId=.*/nodeId=$new_nodeid/g" .env
   sed -i "s/runPort=.*/runPort=$new_port/g" .env
   echo "正在启动"
