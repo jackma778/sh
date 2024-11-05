10ad5c343e205886e72fd580b37be7ec990f27ba
jackma778 <36823039+jackma778@users.noreply.github.com>
2024-11-05 16:24:54 +0900

    Update docker_go.sh

--- docker_go.sh
+++ docker_go.sh
@@ -88,29 +88,33 @@ sed -i "s/nodeId=.*/nodeId=$new_nodeid/g" .env
 sed -i "s/runPort=.*/runPort=$new_port/g" .env
 
 # 优化内核参数 检查是否存在重复配置项
-if ! grep -q "net.ipv4.tcp_congestion_control = bbr" /etc/sysctl.conf; then
-            echo "net.ipv4.tcp_retries2 = 8
-            net.ipv4.tcp_slow_start_after_idle = 0
-            fs.file-max = 1000000
-            net.core.default_qdisc = fq
-            net.ipv4.tcp_congestion_control = bbr
-            fs.inotify.max_user_instances = 8192
-            net.ipv4.tcp_syncookies = 1
-            net.ipv4.tcp_fin_timeout = 30
-            net.ipv4.tcp_tw_reuse = 1
-            net.ipv4.ip_local_port_range = 1024 65000
-            net.ipv4.tcp_max_syn_backlog = 16384
-            net.ipv4.tcp_max_tw_buckets = 6000
-            net.ipv4.route.gc_timeout = 100
-            net.ipv4.tcp_syn_retries = 1
-            net.ipv4.tcp_synack_retries = 1
-            net.core.somaxconn = 32768
-            net.core.netdev_max_backlog = 32768
-            net.ipv4.tcp_timestamps = 0
-            net.ipv4.tcp_max_orphans = 32768
-            net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
-    sysctl -p
-fi
+    if ! grep -q "^net.ipv4.tcp_congestion_control = bbr" /etc/sysctl.conf; then
+        echo "net.core.default_qdisc = fq
+        net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
+        sysctl -p
+    fi
+    if ! grep -q "^net.nf_conntrack_max = 20971520" /etc/sysctl.conf; then
+        echo "fs.file-max = 1000000
+        fs.inotify.max_user_instances = 8192
+        net.core.somaxconn = 32768
+        net.core.netdev_max_backlog = 32768
+        net.ipv4.ip_forward = 1
+        net.ipv4.ip_local_port_range = 10000 65000
+        net.ipv4.route.gc_timeout = 100
+        net.ipv4.tcp_retries2 = 8
+        net.ipv4.tcp_slow_start_after_idle = 0
+        net.ipv4.tcp_syncookies = 1
+        net.ipv4.tcp_fin_timeout = 30
+        net.ipv4.tcp_tw_reuse = 1
+        net.ipv4.tcp_max_syn_backlog = 16384
+        net.ipv4.tcp_max_tw_buckets = 6000
+        net.ipv4.tcp_syn_retries = 1
+        net.ipv4.tcp_synack_retries = 1
+        net.ipv4.tcp_timestamps = 0
+        net.ipv4.tcp_max_orphans = 32768
+        net.nf_conntrack_max = 20971520" >> /etc/sysctl.conf
+        sysctl -p
+    fi
 
 # 优化系统参数 检查是否存在重复配置项
 if ! grep -q "*               soft    nofile           1000000" /etc/security/limits.conf; then
